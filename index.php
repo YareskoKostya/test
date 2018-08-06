a8517385c706df899e9af5b4d5404da2c12071d5
YareskoKostya <zhtcmrj@gmail.com>
2018-08-06 19:40:52 +0300

    third

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
-<?php
-// put your code here
-//echo 'Hello world'."<br/>";
-//print_r("hello");
-//echo "<br/>";
-//var_dump("vasya");
-//phpinfo();
+ <?php
+    // put your code here
+//echo 'Hello world', '<br/>';
+//print_r('Hello Petya');
+ //echo '<br/>';
+ //var_dump('Hello Vasya');
+ //echo '<br/>';
+ //phpinfo();
+ $fruit_1='апельсин';
+ $fruit_2='виноград';
+ $fruit_1_cost=2;
+ $fruit_2_cost=10;
+ $fruit_1_cost=$fruit_2_cost;
+ $fruit_1='ананас';
+ echo 'Название первого фрукта: '.$fruit_1.' Цена первого фрукта: '.$fruit_1_cost.'<br/>';
+ echo 'Название второго фрукта: '.$fruit_2.' Цена второго фрукта: '.$fruit_2_cost.'<br/>';
+ echo 'имя файла '.__FILE__.'<br/>';
+ echo 'на строке '.__LINE__.'<br/>';
 ?>

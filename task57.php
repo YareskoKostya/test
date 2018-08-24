1257bbbabcbe73f8f7f0935ba54b3ec355bca949
YareskoKostya <zhtcmrj@gmail.com>
2018-08-24 17:29:01 +0300

    task57

---
+++ task57.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+ 1. Создать функцию в PHP — getPlus10(), которая будет принимать 
+число и распечатывать сумму этого числа и 10.
+ */
+?>
+<html>
+    <body>
+        <form method="GET">
+            ввести числo<br/>
+            <input type="text" name="a" value="<?php echo $_GET['a']?>"><br/>
+            <input type="submit" value="отправить">
+        </form>
+        <?php
+        $a = $_GET['a'];
+        function getPlus10($n)
+        {
+            return $n + 10;
+        }
+        echo getPlus10($a);
+        ?>
+    </body>
+</html>
